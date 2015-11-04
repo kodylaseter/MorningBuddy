064b708d3234ac816c2cc7e00d311200959db11d
kodylaseter <kodylaseter1@gmail.com>
2015-11-04 13:04:02 -0500

    changed homeactivity settings id thing

--- MyApplication/app/src/main/java/com/example/song/myapplication/HomeActivity.java
+++ MyApplication/app/src/main/java/com/example/song/myapplication/HomeActivity.java
@@ -54,7 +54,8 @@ public class HomeActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.weather_settings) {
+
             return true;
         }
 
